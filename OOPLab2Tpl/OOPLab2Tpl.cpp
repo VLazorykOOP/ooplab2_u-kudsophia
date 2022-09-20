b2df134ddb64f2c22b015156e2fdc8e060f9d35b
vlazoryk <v.lazoryk@chnu.edu.ua>
2022-09-20 19:09:28 +0300

    Добавьте файлы проекта.

---
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -0,0 +1,75 @@
+﻿// OOPLab2Tpl.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+void task1();
+void task2();
+void task3();
+void task4();
+void MenuTask();
+
+int main()
+{
+    cout << "OOP. Template for laboratory work #2.\n";
+
+    char ch = '5';
+    do {
+        system("cls");
+        MenuTask();
+        ch = getchar();
+        getchar();
+        switch (ch) {
+        case '1': task1();   break;
+        case '2': task1();   break;
+        case '3': task1();   break;
+        case '4': task1();   break;
+        case '5': return 0;
+        }
+        cout << " Press any key and enter\n";
+        ch = getchar();
+    } while (ch != 27);
+
+    return 0;
+}
+void MenuTask()
+{
+    cout << "     Menu Task   \n";
+    cout << "    1.  Calculation of expressions using bitwise operations  \n";
+    cout << "    2.  Data encryption using bitwise operations \n";
+    cout << "    3.  Data encryption using structures with bit fields \n";
+    cout << "    4.  The problem of using bitwise operations \n";
+    cout << "    5.  Exit \n";
+}
+void task1() {
+    // Обчислення виразів з використанням побітових операцій
+    // Calculation of expressions using bitwise operations 
+    cout << " Calculation of expressions using bitwise operations  \n";
+}
+
+void task2()
+{
+    // Шифрування даних з використання побітових операцій 
+    // Data encryption using bitwise operations
+    cout << " Data encryption using bitwise operations  \n";
+
+}
+
+void task3()
+{
+    // Шифрування даних з використання стуктур з бітовими полями 
+    // Data encryption using structures with bit fields
+    cout << "  Data encryption using structures with bit fields \n";
+}
+
+
+void task4()
+{   // Задача із використання побітових операцій
+    // The problem of using bitwise operations
+    cout << " Data encryption using structures with bit fields \n";
+
+}
+
+
+
